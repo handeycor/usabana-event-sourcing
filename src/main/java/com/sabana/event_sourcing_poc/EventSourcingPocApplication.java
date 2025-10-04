339faddb52319faf0841dd4136fed3a4b890e9c3
Harver Cordero <harvercordero@gmail.com>
2025-10-04 17:02:06 -0500

    feat: commit inicial-
    Inicialización del proyecto.

---
+++ src/main/java/com/sabana/event_sourcing_poc/EventSourcingPocApplication.java
@@ -0,0 +1,13 @@
+package com.sabana.event_sourcing_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingPocApplication.class, args);
+	}
+
+}
